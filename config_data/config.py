60ece8c61c48cd52100c6f11bb2f4262add06770
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-10-11 10:59:57 +0300

    chore: создать бд

---
+++ config_data/config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+if not find_dotenv():
+	exit('Добавьте файл .env')
+else:
+	load_dotenv()
+
+TOKEN = os.getenv('TOKEN')
+
+base_dir = os.path.dirname(os.path.dirname(__file__))
+DATABASE_URL = 'sqlite:///' + os.path.join(base_dir, 'database', os.getenv('DATABASE_NAME'))
