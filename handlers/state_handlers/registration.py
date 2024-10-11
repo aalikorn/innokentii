c7de983951d6925be6a2c38f3a08c2b777dec03a
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-10-11 14:38:53 +0300

    feat: получаем емейл при регистрации; сохраняем пользователя

--- handlers/state_handlers/registration.py
+++ handlers/state_handlers/registration.py
@@ -5,6 +5,8 @@ import re
 
 from loader import bot, dp
 from states.user_states import UserStates
+from database import crud
+import keyboards
 
 
 @dp.message_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), content_types=['text'],
@@ -36,3 +38,30 @@ async def save_age(message: types.Message, state=FSMContext):
 		await bot.send_message(chat_id=message.from_user.id, text='Отправь свою почту')
 	else:
 		await bot.send_message(message.from_user.id, text='Некорректный формат ввода. Напиши свой возраст')
+
+
+@dp.message_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), content_types=['text'],
+					state=UserStates.wait_mail)
+async def save_user(message: types.Message, state=FSMContext):
+	regex = r'^\S+@\S+\.\S+$'
+
+	await bot.delete_message(message.from_user.id, message.message_id)
+	await bot.delete_message(message.from_user.id, message.message_id - 1)
+	if re.fullmatch(regex, message.text):
+		await state.set_state(state=None)
+		data = await state.get_data()
+		full_name = data.get('user_name')
+		age = data.get('user_age')
+		crud.table_user.add_user(
+			telegram_id=message.from_user.id,
+			full_name=full_name,
+			age=age,
+			mail=message.text
+		)
+		await bot.send_message(
+			message.from_user.id,
+			text='Поздравляем, ты успешно зарегистрировался. Чтобы запустить квест нажми кнопку "Начать"',
+			reply_markup=keyboards.inline.quest.start_quest()
+		)
+	else:
+		await bot.send_message(message.from_user.id, text='Некорректный формат ввода. Напиши свою почту')
