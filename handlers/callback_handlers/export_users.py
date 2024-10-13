82b958b9a961bdfe091b9f99659b23593844660a
Artem037 <artemysttr@gmail.com>
2024-10-13 20:49:52 +0300

    feat: экспорт пользователей по группе

--- handlers/callback_handlers/export_users.py
+++ handlers/callback_handlers/export_users.py
@@ -5,6 +5,9 @@ from aiogram import types
 from loader import bot, dp
 from utils.export_users import export_users
 
+from database import crud
+from states.user_states import UserStates
+
 
 @dp.callback_query_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), text='export_users', state='*')
 async def send_users_xlsx_file(call: types.CallbackQuery, state: FSMContext):
@@ -15,3 +18,10 @@ async def send_users_xlsx_file(call: types.CallbackQuery, state: FSMContext):
 			await bot.send_document(chat_id=call.from_user.id, document=file)
 	except Exception as e:
 		await bot.send_message(call.from_user.id, f"Произошла ошибка при отправке файла: {e}")
+
+
+@dp.callback_query_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), text='export_users_party', state='*')
+async def send_users_party_xlsx_file(call: types.CallbackQuery, state: FSMContext):
+	await state.set_state(UserStates.export_users_party)
+	await call.message.delete()
+	await bot.send_message(call.from_user.id, f"Напиши номер группы, данные пользователей которой надо выгрузить")
--- utils/export_users.py
+++ utils/export_users.py
@@ -10,9 +10,26 @@ def export_users():
 	sheet = workbook.active
 	sheet.title = "user"
 
-	data = [['Фио', 'Возраст', 'Почта', 'Результат']]
+	data = [['Фио', 'Возраст', 'Почта', 'Результат', 'Номер группы']]
 	for user in crud.table_user.get_all():
-		data.append([user.full_name, user.age, user.mail, user.rating])
+		data.append([user.full_name, user.age, user.mail, user.rating, user.party_id])
+
+	for row in data:
+		sheet.append(row)
+
+	base_dir = os.path.dirname(os.path.dirname(__file__))
+	file_name = os.path.join(base_dir, 'utils', 'users.xlsx')
+	workbook.save(file_name)
+	return os.path.join(base_dir, file_name)
+
+
+def export_users_party(party_id: str):
+	workbook = Workbook()
+	sheet = workbook.active
+	sheet.title = "user"
+	data = [['Фио', 'Возраст', 'Почта', 'Результат', 'Номер группы']]
+	for user in crud.table_user.get_all_party(party_id):
+		data.append([user.full_name, user.age, user.mail, user.rating, user.party_id])
 
 	for row in data:
 		sheet.append(row)
