f01a5168225b6e61fb80b069ef7abbcc7e21d4ee
Yaroslav Proskuryakov <yara0yar@mail.ru>
2024-10-14 01:10:14 +0300

    feat: согласие на обработку персональных данных при запуске

--- handlers/command_handlers/start.py
+++ handlers/command_handlers/start.py
@@ -13,10 +13,12 @@ from database import crud
 @dp.message_handler(ChatTypeFilter(chat_type=types.ChatType.PRIVATE), commands=['start'], state='*')
 async def start(message: types.Message, state=FSMContext):
 	await state.set_state(UserStates.start)
-	if message.from_user.id in config.SUPER_ADMINS:
+
+	if message.from_user.id in config.SUPER_ADMINS:  # Пользователь - супер админ
 		text = 'Выберите действие'
 		await bot.send_message(message.from_user.id, text, reply_markup=keyboards.inline.admin.admin_markup())
-	elif message.from_user.id in [i[0] for i in crud.table_admin.get_all()]:
+
+	elif message.from_user.id in [i[0] for i in crud.table_admin.get_all()]:  # Пользователь - экскурсовод
 		text = 'Выберите действие'
 
 		await bot.send_message(
@@ -24,7 +26,8 @@ async def start(message: types.Message, state=FSMContext):
 			text=text,
 			reply_markup=keyboards.inline.admin_tour.admin_tour_markup()
 		)
-	elif crud.table_user.get_user(message.from_user.id):
+
+	elif crud.table_user.get_user(message.from_user.id):  # Пользователь зарегистрирован
 		text = crud.table_main_question.get_main_question(1).content  # первый мейн
 
 		await bot.send_message(
@@ -32,13 +35,11 @@ async def start(message: types.Message, state=FSMContext):
 			text=text,
 			reply_markup=keyboards.inline.quest.start_quest()
 		)
-	else:
-		text = 'Привет! \n' \
-			   'Меня зовут Бот Иннокентий и я провожу ' \
-			   'интерактивные экскурсии по Университету Иннополис.\n' \
-			   'Перед началом работы тебе нужно заполнить анкету.'
+
+	else:  # Пользователь запускает бота впервые
+		text = 'Нажимая "Продолжить", вы соглашаетесь на обработку персональных данных'
 		await bot.send_message(
 			message.from_user.id,
 			text=text,
-			reply_markup=keyboards.inline.registartion.start_registration()
+			reply_markup=keyboards.inline.registartion.accept_personal_data_handling_markup()
 		)
